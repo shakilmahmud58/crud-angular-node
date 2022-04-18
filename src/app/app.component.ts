931e0b5ceb320c9e5f1df27d4c9acca24bc45ab4
Shakil Mahmud <almahmudruet17@gmail.com>
2022-04-18 21:41:52 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './myfile.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
   title = 'angular_project';
+  value ="I am a value";
 }
